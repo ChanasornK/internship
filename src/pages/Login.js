ff76a1ff1bebb196ea89bfeedc015ace3aaa5988
ChanasornK <chanasornhockey@gmail.com>
2024-08-06 21:39:13 +0700

    test Google

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -34,7 +34,7 @@ const Login = () => {
           const credential = error.credential;
           if (errorCode === "auth/account-exists-with-different-credential") {
             alert(
-              "You have already signed up with a different auth provider for that email."
+              "You have already signed up with a different auth provider for that email.."
             );
           } else {
             console.log(error);
